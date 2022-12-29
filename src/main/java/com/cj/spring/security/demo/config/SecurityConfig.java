6f2b41860c4a95da207b9dcd69c5bbabcbbbbfa9
Chinmay Jaiswal <chinmayjaiswalcj@gmail.com>
2022-12-29 20:38:56 +0530

    [demo] adds custom rules for apis authentication

--- src/main/java/com/cj/spring/security/demo/config/SecurityConfig.java
+++ src/main/java/com/cj/spring/security/demo/config/SecurityConfig.java
@@ -1,19 +1,26 @@
 package com.cj.spring.security.demo.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    System.out.println("configuring security checks in custom filter chain...");
-    http.authorizeHttpRequests().anyRequest().authenticated();
-    http.formLogin();
-    http.httpBasic();
+    log.info("configuring security checks in custom filter chain...");
+
+    http.authorizeHttpRequests((authRequest) ->
+            authRequest.requestMatchers("/myAccount").authenticated()
+                .requestMatchers("/contact").permitAll()
+        )
+        .httpBasic().and()
+        .formLogin();
+
     return http.build();
   }
 }
